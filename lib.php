60741f6612b4f897b814e2d6ff0d4e89e8742b8f
Julian Ridden <julian@moodleman.net>
2012-07-23 11:58:35 +1000

    changed function name and various small fixes

--- lib.php
+++ lib.php
@@ -29,7 +29,7 @@
  *
  * @return bool Returns true
  */
-function callback_highlight_uses_sections() {
+function callback_topics_uses_sections() {
     return true;
 }
 
@@ -43,7 +43,7 @@ function callback_highlight_uses_sections() {
  * @param stdClass $modinfo The mod info object for the current course
  * @return bool Returns true
  */
-function callback_highlight_load_content(&$navigation, $course, $coursenode) {
+function callback_topics_load_content(&$navigation, $course, $coursenode) {
     return $navigation->load_generic_course_sections($course, $coursenode, 'topics');
 }
 
@@ -53,23 +53,13 @@ function callback_highlight_load_content(&$navigation, $course, $coursenode) {
  *
  * @return string
  */
-function callback_highlight_definition() {
+function callback_topics_definition() {
     return get_string('topic');
 }
 
-/**
- * The GET argument variable that is used to identify the section being
- * viewed by the user (if there is one)
- *
- * @return string
- */
-function callback_highlight_request_key() {
-    return 'topic';
-}
-
-function callback_highlight_get_section_name($course, $section) {
+function callback_topics_get_section_name($course, $section) {
     // We can't add a node without any text
-    if (!empty($section->name)) {
+    if ((string)$section->name !== '') {
         return format_string($section->name, true, array('context' => get_context_instance(CONTEXT_COURSE, $course->id)));
     } else if ($section->section == 0) {
         return get_string('section0name', 'format_topics');
@@ -84,7 +74,7 @@ function callback_highlight_get_section_name($course, $section) {
  * @see course_format_ajax_support()
  * @return stdClass
  */
-function callback_highlight_ajax_support() {
+function callback_topics_ajax_support() {
     $ajaxsupport = new stdClass();
     $ajaxsupport->capable = true;
     $ajaxsupport->testedbrowsers = array('MSIE' => 6.0, 'Gecko' => 20061111, 'Safari' => 531, 'Chrome' => 6.0);
@@ -92,12 +82,22 @@ function callback_highlight_ajax_support() {
 }
 
 /**
- * Returns a URL to arrive directly at a section
+ * Callback function to do some action after section move
  *
- * @param int $courseid The id of the course to get the link for
- * @param int $sectionnum The section number to jump to
- * @return moodle_url
+ * @param stdClass $course The course entry from DB
+ * @return array This will be passed in ajax respose.
  */
-function callback_highlight_get_section_url($courseid, $sectionnum) {
-    return new moodle_url('/course/view.php', array('id' => $courseid, 'topic' => $sectionnum));
+function callback_topics_ajax_section_move($course) {
+    global $COURSE, $PAGE;
+
+    $titles = array();
+    rebuild_course_cache($course->id);
+    $modinfo = get_fast_modinfo($COURSE);
+    $renderer = $PAGE->get_renderer('format_topics');
+    if ($renderer && ($sections = $modinfo->get_section_info_all())) {
+        foreach ($sections as $number => $section) {
+            $titles[$number] = $renderer->section_title($section, $course);
+        }
+    }
+    return array('sectiontitles' => $titles, 'action' => 'move');
 }
